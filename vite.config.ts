f48c9656cbfa2446bb32e07dcdfd0c9d37a695a4
BalaMurugan-2005 <pbalamurugan260@gmail.com>
2025-11-13 20:44:23 +0530

    ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
 })
-
